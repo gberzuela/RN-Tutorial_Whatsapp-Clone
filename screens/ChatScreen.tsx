e2a500072c108ea9ba7376a5c988cdd49e7ef472
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-12-30 00:11:32 -0800

    adds ChatScreen component

---
+++ screens/ChatScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { View } from '../components/Themed';
+
+import ChatListItem from '../components/ChatListItem';
+
+export default function ChatScreen() {
+	return (
+		<View style={styles.container}>
+			<ChatListItem chatRoom={{ lastMessage: { content: 'Hello World!' } }} />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
