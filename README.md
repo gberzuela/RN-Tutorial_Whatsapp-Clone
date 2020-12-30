b66dbb468401ba1ee716dbbc9dc3d8a2e3aaaf4a
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-12-29 23:14:46 -0800

    adds navigation for camera, chats, status, and call tabs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # RN-Tutorial_Whatsapp-Clone
 
-Realtime chat app built in react native following Vadim Savin's YouTube video:
+Realtime chat app built in react native, initialized with expo's 'tabs' template, and followed Vadim Savin's YouTube video:
 
 [Vadim Savin's Tutorial](https://www.youtube.com/watch?v=EvSUJ5lUcBw&list=TLPQMzAxMjIwMjBlSroaYZfirA&index=3&ab_channel=VadimSavin)
 
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,23 +1,36 @@
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import {
+	NavigationContainer,
+	DefaultTheme,
+	DarkTheme,
+} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import { ColorSchemeName, View } from 'react-native';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
+import MainTabNavigator from './MainTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+import Colors from '../constants/Colors';
+
+import { Octicons, MaterialCommunityIcons } from '@expo/vector-icons';
+// ^^ icons.expo.fyi
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
-  return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
-    </NavigationContainer>
-  );
+export default function Navigation({
+	colorScheme,
+}: {
+	colorScheme: ColorSchemeName;
+}) {
+	return (
+		<NavigationContainer
+			linking={LinkingConfiguration}
+			theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+		>
+			<RootNavigator />
+		</NavigationContainer>
+	);
 }
 
 // A root stack navigator is often used for displaying modals on top of all other content
@@ -25,10 +38,51 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
 const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: Colors.light.tint,
+					shadowOpacity: 0, // removes header view border on iOS
+					elevation: 0, // removes header view border on android
+				},
+				headerTintColor: Colors.light.background,
+				headerTitleAlign: 'left',
+				headerTitleStyle: {
+					fontWeight: 'bold',
+				},
+			}}
+		>
+			<Stack.Screen
+				name="Root"
+				component={MainTabNavigator}
+				options={{
+					title: 'WhatsApp',
+					// vv displays the icons for the right side of the header
+					headerRight: () => (
+						<View
+							style={{
+								flexDirection: 'row',
+								width: 60,
+								justifyContent: 'space-between',
+								marginRight: '10',
+							}}
+						>
+							<Octicons name="search" size={22} color="white" />
+							<MaterialCommunityIcons
+								name="dots-vertical"
+								size={22}
+								color="white"
+							/>
+						</View>
+					),
+				}}
+			/>
+			<Stack.Screen
+				name="NotFound"
+				component={NotFoundScreen}
+				options={{ title: 'Oops!' }}
+			/>
+		</Stack.Navigator>
+	);
 }
